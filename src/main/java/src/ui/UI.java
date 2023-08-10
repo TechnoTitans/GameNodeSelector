01f27776be43faf064beeb32e6e50e572f35d6ba
Max <maxgoog06@outlook.com>
2023-08-10 11:19:32 -0400

    changed ui to look better

--- src/main/java/src/ui/UI.java
+++ src/main/java/src/ui/UI.java
@@ -5,19 +5,18 @@ import src.NTListener;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.util.EnumSet;
 
 public class UI extends JFrame {
     private final NTListener ntListener;
 
-    private JComboBox<String> autoSelector;
-    private JComboBox<String> profileSelector;
-    private JLabel autonomousLabel;
-    private JLabel driverLabel;
-    private JMenu jMenu1;
-    private JMenuBar jMenuBar1;
-    private JMenuItem jMenuItem1;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JMenuItem refreshMenuItem;
+    private javax.swing.JLabel autonomousLabel;
+    private javax.swing.JComboBox<String> autoSelector;
+    private javax.swing.JLabel profileLabel;
+    private javax.swing.JComboBox<String> profileSelector;
 
     public UI(final NTListener ntListener) {
         this.ntListener = ntListener;
@@ -28,104 +27,117 @@ public class UI extends JFrame {
 
             ntListener.getNetworkTableInstance().addListener(
                     ntListener.getAutoSubscriber(),
-                    EnumSet.of(NetworkTableEvent.Kind.kPublish),
+                    EnumSet.of(NetworkTableEvent.Kind.kTopic),
                     (event) -> populateAutonomousPaths()
+            );
+
+            ntListener.getNetworkTableInstance().addListener(
+                    ntListener.getProfileSubscriber(),
+                    EnumSet.of(NetworkTableEvent.Kind.kTopic),
+                    (event) -> populateDriverProfiles()
+            );
+
+            ntListener.getNetworkTableInstance().addConnectionListener(
+                    true,
+                    (event) -> setTitle("TitanDash | " +
+                            (event.is(NetworkTableEvent.Kind.kConnected)
+                                    ? "Connected"
+                                    : event.is(NetworkTableEvent.Kind.kDisconnected)
+                                    ? "Disconnected"
+                                    : "Unknown"
+                            )
+                    )
             );
         });
     }
 
     private void initComponents() {
-        autoSelector = new JComboBox<>();
-        profileSelector = new JComboBox<>();
-        autonomousLabel = new JLabel();
-        driverLabel = new JLabel();
-        jMenuBar1 = new JMenuBar();
-        jMenu1 = new JMenu();
-        jMenuItem1 = new JMenuItem();
+        autoSelector = new javax.swing.JComboBox<>();
+        profileSelector = new javax.swing.JComboBox<>();
+        autonomousLabel = new javax.swing.JLabel();
+        profileLabel = new javax.swing.JLabel();
+        menuBar = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        refreshMenuItem = new javax.swing.JMenuItem();
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setTitle("TitanDash");
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("TitanDashboard | Unknown");
+        setMinimumSize(new java.awt.Dimension(800, 308));
 
-        autoSelector.setToolTipText("Select Autonomous");
-        autoSelector.addActionListener(this::autoSelectedEvent);
+        autoSelector.addActionListener(this::autoSelectorActionPerformed);
         populateAutonomousPaths();
 
-        profileSelector.setToolTipText("Select Driver Profile");
-        profileSelector.addActionListener(this::profileSelectedEvent);
+        profileSelector.addActionListener(this::profileSelectorActionPerformed);
         populateDriverProfiles();
 
-        autonomousLabel.setFont(new java.awt.Font("Segoe UI", Font.PLAIN, 14));
-        autonomousLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        autonomousLabel.setFont(new java.awt.Font("Segoe UI", Font.PLAIN, 14)); // NOI18N
+        autonomousLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         autonomousLabel.setText("Autonomous");
 
-        driverLabel.setFont(new java.awt.Font("Segoe UI", Font.PLAIN, 14));
-        driverLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        driverLabel.setText("Driver Profile");
+        profileLabel.setFont(new java.awt.Font("Segoe UI", Font.PLAIN, 14)); // NOI18N
+        profileLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        profileLabel.setText("Profile Selector");
 
-        jMenu1.setText("File");
+        fileMenu.setText("File");
 
-        jMenuItem1.setText("Refresh");
-        jMenuItem1.addActionListener(this::refreshButtonPressed);
-        jMenu1.add(jMenuItem1);
+        refreshMenuItem.setText("Refresh");
+        refreshMenuItem.addActionListener(this::refreshMenuItemActionPerformed);
+        fileMenu.add(refreshMenuItem);
 
-        jMenuBar1.add(jMenu1);
+        menuBar.add(fileMenu);
 
-        setJMenuBar(jMenuBar1);
+        setJMenuBar(menuBar);
 
-        final GroupLayout layout = new GroupLayout(getContentPane());
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addGap(50, 50, 50)
-                                .addComponent(autoSelector, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(122, 122, 122)
-                                .addComponent(profileSelector, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(50, 50, 50))
-                        .addGroup(layout.createSequentialGroup()
-                                .addContainerGap(140, Short.MAX_VALUE)
-                                .addComponent(autonomousLabel, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 300, Short.MAX_VALUE)
-                                .addComponent(driverLabel, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(140, Short.MAX_VALUE))
+                                .addContainerGap(106, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(autoSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(autonomousLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 107, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(profileLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(profileSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(106, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addGap(37, 37, 37)
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                        .addComponent(autonomousLabel, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(driverLabel, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                        .addComponent(autoSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(profileSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(432, Short.MAX_VALUE))
+                                .addGap(52, 52, 52)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(autonomousLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(profileLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(autoSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(profileSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(477, Short.MAX_VALUE))
         );
-
         pack();
     }
 
     //Listeners
-    private void refreshButtonPressed(final ActionEvent evt) {
+    private void refreshMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
         populateAutonomousPaths();
         populateDriverProfiles();
     }
 
-    private void autoSelectedEvent(final ActionEvent evt) {
+    private void autoSelectorActionPerformed(java.awt.event.ActionEvent evt) {
         final String selectedAuto = (String) autoSelector.getSelectedItem();
         if (selectedAuto != null) {
             ntListener.selectAuto(selectedAuto);
         }
     }
 
-    private void profileSelectedEvent(final ActionEvent evt) {
+    private void profileSelectorActionPerformed(java.awt.event.ActionEvent evt) {
         final String selectedProfile = (String) profileSelector.getSelectedItem();
         if (selectedProfile != null) {
             ntListener.selectProfile(selectedProfile);
         }
     }
-
 
     private void populateAutonomousPaths() {
         autoSelector.removeAllItems();
