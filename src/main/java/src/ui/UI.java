771ec01d630460cf9e656b2cd36259c694164829
Max <maxgoog06@outlook.com>
2023-08-11 15:14:42 -0400

    Made shorter.

--- src/main/java/src/ui/UI.java
+++ src/main/java/src/ui/UI.java
@@ -4,7 +4,6 @@ import edu.wpi.first.networktables.NetworkTableEvent;
 import src.NTListener;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.EnumSet;
@@ -83,7 +82,8 @@ public class UI extends JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("TitanDash | Disconnected");
-        setMinimumSize(new Dimension(800, 308));
+        setMinimumSize(new Dimension(800, 450));
+        setPreferredSize(new Dimension(800, 450));
 
         autoSelector.addActionListener(this::autoSelectorActionPerformed);
         populateAutonomousPaths();
@@ -101,7 +101,7 @@ public class UI extends JFrame {
 
         gridLayout.setHorizontalAlignment(SwingConstants.CENTER);
         gridLayout.setIcon(new ImageIcon(
-                Objects.requireNonNull(getClass().getResource("/grids/gridlayout.png")))
+                Objects.requireNonNull(getClass().getResource("/grids/gridlayout-UNKNOWN.png")))
         );
 
         fileMenu.setText("File");
@@ -121,16 +121,38 @@ public class UI extends JFrame {
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap(106, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(autoSelector, GroupLayout.PREFERRED_SIZE, 287, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(autonomousLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(
+                                                autoSelector,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                287,
+                                                GroupLayout.PREFERRED_SIZE
+                                        )
+                                        .addComponent(
+                                                autonomousLabel,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                Short.MAX_VALUE
+                                        ))
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 106, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(profileLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(profileSelector, GroupLayout.PREFERRED_SIZE, 287, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(
+                                                profileLabel,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                Short.MAX_VALUE
+                                        )
+                                        .addComponent(
+                                                profileSelector,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                287,
+                                                GroupLayout.PREFERRED_SIZE
+                                        ))
                                 .addContainerGap(107, Short.MAX_VALUE))
                         .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(gridLayout, GroupLayout.PREFERRED_SIZE, 783, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(
+                                        gridLayout, GroupLayout.PREFERRED_SIZE, 783, GroupLayout.PREFERRED_SIZE
+                                )
                                 .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -138,15 +160,37 @@ public class UI extends JFrame {
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(52, 52, 52)
                                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                        .addComponent(autonomousLabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(profileLabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(
+                                                autonomousLabel,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                27,
+                                                GroupLayout.PREFERRED_SIZE
+                                        )
+                                        .addComponent(
+                                                profileLabel,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                27,
+                                                GroupLayout.PREFERRED_SIZE
+                                        ))
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                        .addComponent(autoSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(profileSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
-                                .addComponent(gridLayout, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE)
-                                .addGap(65, 65, 65))
+                                        .addComponent(
+                                                autoSelector,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE
+                                        )
+                                        .addComponent(
+                                                profileSelector,
+                                                GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE
+                                        ))
+                                .addGap(80, 80, 80)
+                                .addComponent(
+                                        gridLayout, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE
+                                )
+                                .addContainerGap(81, Short.MAX_VALUE))
         );
 
         pack();
